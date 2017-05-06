29301f8cff1121dcfbfac3dc5f62f06dc1bec78e
victorwltsang <victorwltsang@gmail.com>
2017-05-06 09:15:45 -0700

    heroku sql

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,25 @@
 // Setup up MySql Connection
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection.mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: "3306",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
+
+
 
 // Test Connection
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as", connection.threadId);
-});
+connection.connect();
+
 
 // export connection for ORM
 module.exports = connection;
+nnection;
